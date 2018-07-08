936f323fb6570232df94453cac2f943023637a0d
necatiozmen$ git config --global user.email necati.ozmen.9@gmail.comgit configgit config --global user.name necatiozmen <necati.ozmen.9@gmail.com>
2018-07-08 23:47:44 +0300

    feat:Speech to text works

--- controller.js
+++ controller.js
@@ -2,6 +2,9 @@ const record = require('node-record-lpcm16');
 const speech = require('@google-cloud/speech');
 const client = new speech.SpeechClient();
 
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 const filename = 'Local path to audio file, e.g. /path/to/audio.raw';
 const encoding = 'LINEAR16';
@@ -18,20 +21,18 @@ const request = {
   interimResults: false, // If you want interim results, set this to true
 };
 
-// res.set({
-//   'Cache-Control': 'no-store'
-// });
 
 exports.speechcall = (req, res, next) => {
-
+    let text ='ver';
     const recognizeStream = client
     .streamingRecognize(request)
     .on('error', console.error)
-    .on('data', data => res.send(data.results[0] && data.results[0].alternatives[0]
+    .on('data', data => text = data.results[0] && data.results[0].alternatives[0]
         ? `Transcription: ${data.results[0].alternatives[0].transcript}` : `
 Reached transcription time limit, press Ctrl+C
-`)
+`
 );
+
 
     record
       .start({
@@ -45,11 +46,25 @@ Reached transcription time limit, press Ctrl+C
     })
       .on('error', console.error)
       .pipe(recognizeStream);
-      console.log('Listening, press Ctrl+C to stop.');
+    console.log('Listening, press Ctrl+C to stop.');
+
+
 
   };
 
-  exports.test = (req,res) => {
-
-    res.send('tekrar dene');
-  }
+exports.test = (req, res) => {
+  // console.log('');
+  // io.on('connection', socket => {
+  //   console.log(socket.id);
+  //
+  //   socket.on('SEND_MESSAGE', data => {
+  //     console.log(data);
+  //     io.emit('SEND_MESSAGE', text);
+  //   });
+  //
+  //   socket.on('disconnect', () => {
+  //     console.log('user disconnected');
+  //   });
+  // });
+  res.send('tekrar dene');
+};
