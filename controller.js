0542543ca17b953f823792c7648b2bb0594e8c15
necatiozmen$ git config --global user.email necati.ozmen.9@gmail.comgit configgit config --global user.name necatiozmen <necati.ozmen.9@gmail.com>
2018-07-20 14:05:57 +0300

    refac:Routes seperate to controllers

--- controller.js
+++ controller.js
@@ -1,65 +1,85 @@
 const record = require('node-record-lpcm16');
 const speech = require('@google-cloud/speech');
 const client = new speech.SpeechClient();
-
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-
+const io = require('./index');
 const Translate = require('@google-cloud/translate');
 const translate = new Translate();
 
-const text = 'NEW, world!';
-const target = 'tr';
+const filename = 'Local path to audio file, e.g. /path/to/audio.raw';
+const encoding = 'LINEAR16';
+const sampleRateHertz = 16000;
+const languageCode = 'en-US';
 
-exports.speechcall = (req, res, next) => {
-    let text = 'ver';
+let target = 'tr';
+let text = '';
+
+let request = {
+  config: {
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+  },
+  single_utterance: true,
+  interimResults: false,
+};
+
+exports.speechLang = (req, res, next) => {
+    console.log('controller req', req.query);
+    req.query.speaker ? request.config.languageCode = req.query.speaker : '';
+    req.query.translation ?  target = req.query.translation : '';
+    res.send(JSON.stringify(req.query));
+  };
+
+exports.speechRecord = (req, res, next) => {
     const recognizeStream = client
     .streamingRecognize(request)
     .on('error', console.error)
-    .on('data', data => process.stdout.write(data.results[0] && data.results[0].alternatives[0]
-        ? `Transcription: ${data.results[0].alternatives[0].transcript}` : `
-    Reached transcription time limit, press Ctrl+C
-    `
-  ));
-
+    .on('data', data => text = data.results[0] && data.results[0].alternatives[0]
+        ? `${data.results[0].alternatives[0].transcript}` : `
+      Reached transcription time limit, press Ctrl+C
+      `
+  );
     record
       .start({
       sampleRateHertz: sampleRateHertz,
       threshold: 0,
-      // thresholdEnd:1,
-      // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
       verbose: false,
-      recordProgram: 'rec', // Try also "arecord" or "sox"
+      recordProgram: 'rec',
       silence: '10.0',
     })
       .on('error', console.error)
       .pipe(recognizeStream);
     console.log('Listening, press Ctrl+C to stop.');
-
-    res.send('only test');
+    res.send(JSON.stringify('tr'));
   };
 
-exports.test = (req, res, next) => {
-  res.send('test controller');
-  translate
-  .translate(text, target)
-  .then(results => {
-    let translations = results[0];
-
-    translations = Array.isArray(translations)
-      ? translations
-      : [translations];
-
-    console.log(`Translations: ${translations}`);
-    translations.forEach((translation, i) => {
-      res.send(`${text[i]} => (${target}) ${translation}`);
+exports.socketConnect = (req, res, next) => {
+  io.io.set('origins', 'http://localhost:3000');
+  io.io.on('connection', socket => {
+    console.log(socket.id);
+    socket.on('SEND_TEXT_MESSAGE', data => {
+      data.socketId = socket.id,
+      io.io.emit('SEND_MESSAGE_TOCLIENT', data);//send text which is coming from client to all client listeners
     });
-    return 'internal promise';
-  })
-  .then(el => console.log(el))
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
 
+    socket.on('GET_SPEECH_TEXT', data => {
+      data.text ? text = data.text : ''; //if something written in input
+      let translatedText = '';
+      translate
+      .translate(text, target)
+      .then(results => {
+        let translations = results[0];
+        console.log(`Translations: ${translations}`);
+        return translations;
+      })
+      .then(transText => io.io.emit('GET_SPEECH_TEXT', `${text} => ${transText}`))
+      .catch(err => {
+        console.error('ERROR:', err);
+      });
+    });
+
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
 };
